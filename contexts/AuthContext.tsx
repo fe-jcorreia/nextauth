b5f3eace7b04b45c9d5541b19113d333932d71da
fe-jcorreia <fe.jcorreia@gmail.com>
2021-05-04 16:45:10 -0300

    authentication context added

---
+++ contexts/AuthContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, ReactNode, useState } from "react";
+import { api } from "../services/api";
+import Router from "next/router";
+
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
+
+type SignInCredentials = {
+  email: string;
+  password: string;
+};
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentials): Promise<void>;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>();
+
+  const isAuthenticated = false;
+
+  async function signIn({ email, password }: SignInCredentials) {
+    try {
+      const response = await api.post("sessions", { email, password });
+      const { permissions, roles } = response.data;
+      setUser({
+        email,
+        permissions,
+        roles,
+      });
+      Router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import { FormEvent, useContext, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { AuthContext } from "../contexts/AuthContext";
+
+export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const data = { email, password };
+    await signIn(data);
+  }
+
+  return (
+    <form className={styles.container}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit" onSubmit={handleSubmit}>
+        Entrar
+      </button>
+    </form>
+  );
+}
